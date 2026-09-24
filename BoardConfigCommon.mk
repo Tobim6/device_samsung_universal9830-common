268be8ba2aea3d26d3b241995e2e316f8cdfa22d
Tobim6 <102646588+Tobim6@users.noreply.github.com>
2026-09-24 11:09:03 +0200

    common: exclude unused libGeoTrans10 prebuilt

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -118,6 +118,9 @@ TARGET_USES_PREBUILT_SECRIL_CLIENT := true
 
 # Camera/scaler/jpeg BSP libs come from stock blobs, not the Make sources
 TARGET_USES_PREBUILT_EXYNOS_BSP_LIBS := true
+
+# libGeoTrans10 is unused and its geoTransService interface is not built from source
+TARGET_EXCLUDES_GEOTRANS := true
 TARGET_BOARD_PLATFORM := universal990
 TARGET_BOOTLOADER_BOARD_NAME := exynos990
 TARGET_SOC := exynos990
